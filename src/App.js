be1e4f395349dee2b361a2e0ba0a96e01968cee4
Andres Higuita <yecid3257@live.com>
2021-04-08 21:15:40 -0500

    solve issue

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { checked: false };
+    this.handleShow = this.handleShow.bind(this);
+  }
+
   render() {
+    const content = this.state.checked ? (
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    ) : null;
+
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label>
+          <input
+            type="checkbox"
+            checked={this.state.checked}
+            onChange={this.handleShow}
+          />
+          Mostrar información importante
+        </label>
+        {content}
       </div>
     );
+  }
+  handleShow() {
+    this.setState({ checked: !this.state.checked });
   }
 }
 
